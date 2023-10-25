91046dbe8f574b03902162cb12a9532040d1be60
thoin <thoino@outlook.fr>
2023-10-25 17:23:10 +0200

    __ini__, depth, fromFile, creation new_node, TEST OK

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from .quadtree import QuadTree, TkQuadTree
+
--- src/quadtree.py
+++ src/quadtree.py
@@ -1,23 +1,52 @@
 from __future__ import annotations
+import json
+import os.path
+
+
 class QuadTree:
-    NB_NODES : int = 4
-    def __init__(self, hg: bool | QuadTree, hd: bool | QuadTree, bd: bool | QuadTree,bg: bool | QuadTree):
-        pass
+    NB_NODES: int = 4
+
+    def __init__(self, hg: bool | QuadTree, hd: bool | QuadTree, bd: bool | QuadTree, bg: bool | QuadTree):
+        self.__blocks = [hg, hd, bd, bg]
 
     @property
     def depth(self) -> int:
         """ Recursion depth of the quadtree"""
-        return 1
+        max_depth = 0
+
+        for block in self.__blocks:
+            if isinstance(block, QuadTree) and block.depth > max_depth:
+                max_depth = block.depth
+        return max_depth + 1
 
     @staticmethod
     def fromFile(filename: str) -> QuadTree:
         """ Open a given file, containing a textual representation of a list"""
-        pass
+        try:
+            with open(filename, "r") as f:
+                lst = json.load(f)
+        except Exception as e:
+            print("fromfile() error : " + str(e))
+
+        return QuadTree.new_node(lst)
+
+
+
+    @staticmethod
+    def new_node(qt_list):
+        qt_param = []
+        for element in qt_list:
+            if not isinstance(element, list):
+                qt_param.append(element)
+            else:
+                qt_param.append(QuadTree.new_node(element))
+        return QuadTree(qt_param[0], qt_param[1], qt_param[2], qt_param[3])
 
     @staticmethod
     def fromList(data: list) -> QuadTree:
         """ Generates a Quadtree from a list representation"""
         pass
+
 
 class TkQuadTree(QuadTree):
     def paint(self):
