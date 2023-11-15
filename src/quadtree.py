1a43c8d917a7f220e0b436642797eb665ab47bb7
thoin <thoino@outlook.fr>
2023-11-15 17:04:01 +0100

    corrections pylint

--- src/quadtree.py
+++ src/quadtree.py
@@ -1,13 +1,20 @@
+""" allow to add a Quadtree parameter in the Quadtree constructor """
 from __future__ import annotations
-from param import *
+import sys
 import json
-from tkinter import *
+from tkinter import Tk, Frame
+from param import MAX_SIZE, color_dict
 
 
 class QuadTree:
+    """ Quadtree """
     NB_NODES: int = 4
+    """ number of node of a Quadtree, set 8 for a 3D Quadtree """
 
-    def __init__(self, hg: bool | QuadTree, hd: bool | QuadTree, bd: bool | QuadTree, bg: bool | QuadTree):
+    def __init__(self, hg: bool | QuadTree,
+                 hd: bool | QuadTree,
+                 bd: bool | QuadTree,
+                 bg: bool | QuadTree):
         """
         hg: top left block
         hd: top rigth block
@@ -27,38 +34,38 @@ class QuadTree:
         return max_depth + 1
 
     @staticmethod
-    def isFormatCorrect(data: list) -> bool:
+    def is_format_correct(data: list) -> bool:
         """ Check if the file is in a Quadtree list format """
         if isinstance(data, list):
             if len(data) != QuadTree.NB_NODES:
                 return False
             for element in data:
-                if not QuadTree.isFormatCorrect(element):
+                if not QuadTree.is_format_correct(element):
                     return False
         return True
 
     @staticmethod
-    def fromFile(filename: str) -> QuadTree:
+    def from_file(filename: str) -> QuadTree:
         """ Open a given file, containing a textual representation of a list,
          check if the list is in a Quatree format, if not close the app"""
         try:
-            with open(filename, "r") as f:
+            with open(filename, "r", encoding='utf8') as f:
                 lst = json.load(f)
         except FileNotFoundError as e:
-            print("fromfile() error : " + str(e))
+            print("from_file() error : " + str(e))
 
-        if not QuadTree.isFormatCorrect(lst):
+        if not QuadTree.is_format_correct(lst):
             print(f"bad file format : {filename}")
-            exit()
-        return QuadTree.fromList(lst)
+            sys.exit()
+        return QuadTree.from_list(lst)
 
     @staticmethod
-    def fromList(qt_list: list) -> QuadTree:
+    def from_list(qt_list: list) -> QuadTree:
         """ Generates a Quadtree from a list representation """
         qt_param = []
         for element in qt_list:
             if isinstance(element, list):
-                qt_param.append(QuadTree.fromList(element))
+                qt_param.append(QuadTree.from_list(element))
             else:
                 qt_param.append(element)
         return QuadTree(*qt_param)
@@ -76,7 +83,7 @@ class TkQuadTree(Tk):
     """ block placement order, must match the order of the Quadtree constructor """
 
     @staticmethod
-    def newQuadtreeFrame(master_frame: Frame, qt: QuadTree, depth: int = 0):
+    def new_quadtree_frame(master_frame: Frame, qt: QuadTree, depth: int = 0):
         """
         Create 4 new frames inside master_frame
         master_frame : main frame in which will be created 4 frame
@@ -91,7 +98,7 @@ class TkQuadTree(Tk):
             if isinstance(element, QuadTree):
                 frame = Frame(master_frame, width=lenght, height=lenght)
                 frame.place(x=x_pos, y=y_pos)
-                TkQuadTree.newQuadtreeFrame(frame, element, depth)
+                TkQuadTree.new_quadtree_frame(frame, element, depth)
             else:
                 frame = Frame(master_frame, bg=color_dict[element], width=lenght, height=lenght)
                 frame.place(x=x_pos, y=y_pos)
@@ -99,7 +106,7 @@ class TkQuadTree(Tk):
     def __init__(self, filename: str):
         """ Initiate a TKinter windows with a representation of a Quadtree (filename) """
         super().__init__()
-        self.__quadtree = QuadTree.fromFile(filename)
+        self.__quadtree = QuadTree.from_file(filename)
         self.geometry(f"{MAX_SIZE}x{MAX_SIZE}")
-        self.newQuadtreeFrame(self, self.__quadtree)
+        self.new_quadtree_frame(self, self.__quadtree)
         self.title(f"{filename} | Depth : {str(self.__quadtree.depth)} layer(s)")
